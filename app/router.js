5297f4b18462f704ec9552d06fe054a647bdcbb7
Jeffrey Biles <jeffreybiles@Jeffreys-MacBook-Pro-2.local>
2014-04-24 13:38:04 -0500

    removed EAK example code

--- app/router.js
+++ app/router.js
@@ -1,12 +1,6 @@
 var Router = Ember.Router.extend(); // ensure we don't share routes between all Router instances
 
 Router.map(function() {
-  this.route('component-test');
-  this.route('helper-test');
-  // this.resource('posts', function() {
-  //   this.route('new');
-  // });
-
   this.resource('about');
   this.resource('posts', function() {
     this.resource('post', { path: ':post_id' });
