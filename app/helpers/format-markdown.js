66c9162425958b8207bc8988ace4f7dd5225024a
jeffreybiles <bilesjeffrey@gmail.com>
2014-05-17 01:23:26 -0500

    Bootstrap!

--- app/helpers/format-markdown.js
+++ app/helpers/format-markdown.js
@@ -1,4 +1,6 @@
 export default Ember.Handlebars.makeBoundHelper(function(input) {
-  var showdown = new Showdown.converter();
-  return new Handlebars.SafeString(showdown.makeHtml(input));
+  if(input){
+    var showdown = new Showdown.converter();
+    return new Handlebars.SafeString(showdown.makeHtml(input));
+  }
 });
